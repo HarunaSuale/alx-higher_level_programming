afb02cb7a845bb771e1210ec23d2398bbf02b526
Haruna Suale <harunasuale10@gmail.com>
2024-01-21 06:46:54 -0800

    task 6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,4 +1,6 @@
- in range(0, 8):
-         for j in range(i + 1, 10):
-                     print("{:d}{:d}".format(i, j), end=', ')
-                     print("{:d}{:d}".format(i + 1, j))
+#!/usr/bin/python3
+
+for i in range(0, 8):
+    for j in range(i + 1, 10):
+        print("{:d}{:d}".format(i, j), end=', ')
+print("{:d}{:d}".format(i + 1, j))
