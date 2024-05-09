b08dc76edfcb83fdb0ad523b6c658883e730727c
Haruna Suale <harunasuale10@gmail.com>
2024-05-09 15:33:07 -0700

    scripts

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-#bash script that takes in a URL, send a request to the URL, and display the side of the body of the responds
-
+# Bash script that take in a URL, sends a request to it, and displays the size of the body of the response, size should be displayed in bytes
 curl -s "$1" | wc -c
