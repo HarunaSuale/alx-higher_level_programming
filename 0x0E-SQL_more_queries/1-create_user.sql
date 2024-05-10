65252835c5df23814df6bd35da3b115a0c530379
Haruna Suale <harunasuale10@gmail.com>
2024-05-10 14:01:40 -0700

    done

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,6 +1,6 @@
 --Create a new user user_0d_1
 --grant the user all permissions
 
-CREATE USER IF NOT EXISTS "user_0d_1"@"localhost" IDENTIFIED BY "user_0d_1_pwd";
-GRANT ALL PRIVILEGES ON *.* TO "user_0d_1"@"localhost";
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
 FLUSH PRIVILEGES; 
